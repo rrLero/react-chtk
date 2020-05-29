c78fbcdcd5dcfa5393a3405f1b68bbe1497e1ba1
rrLero <rrlero5@gmail.com>
2020-05-29 22:40:53 +0300

    update sponsors logo, add laska, update stadion image

--- src/app/components/partners/index.js
+++ src/app/components/partners/index.js
@@ -9,9 +9,14 @@ const Partners = ({}: Props) => {
         <article id="partners" className="partners">
             <h1 className="partners__title">Наши партнеры</h1>
             <h4 className="partners__sub-title">Ми благодарны нашим партнерам за поддержку</h4>
-            <a target="_blank" href="http://www.babolat.ua/" name="babolat-site" rel="noopener">
-                <img src={require('./Babolat-Logo.jpg')} className="partners__image" alt="jet-brains-logo"/>
-            </a>
+            <div className="partners__list">
+                <a target="_blank" href="http://www.babolat.ua/" name="babolat-site" rel="noopener">
+                    <img src={require('./babolat.png')} className="partners__image" alt="babolat-logo"/>
+                </a>
+                <a target="_blank" href="http://laska.ua/" name="laska-site" rel="noopener">
+                    <img src={require('./logo-laska.png')} className="partners__image" alt="laska-logo"/>
+                </a>
+            </div>
         </article>
     )
 };
